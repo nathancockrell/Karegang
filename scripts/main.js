a9af30273cfca0b168713da33efee88c260ef7e9
Nathan <nathanbc55@gmail.com>
2023-02-12 16:18:31 -0600

    uhoh

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+
+const canvas = document.getElementById("canvas");
+        let ctx = canvas.getContext("2d");
+        ctx.canvas.width = window.innerHeight-20;
+        ctx.canvas.height = window.innerHeight-20;
