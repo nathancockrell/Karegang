dfcef904bacf70f8bda2170c59a6143a99479992
JustinCockrel1 <105675389+JustinCockrel1@users.noreply.github.com>
2023-02-12 21:18:49 -0600

    start basic screen stuff

--- scripts/KareGang.js
+++ scripts/KareGang.js
@@ -1,9 +1,39 @@
 import Game from "../WatawaeEngine/Game.js";
+import Gangster from "./Gangster.js";
 
 export default class KareGang extends Game {
 
+    constructor(ctx) {
+        super(ctx);
+
+        this.gangster = new Gangster(3, 3, 0.1,0.1);
+    }
+
     update(elapsedTime) {
         // console.log(elapsedTime);
+
+        this.tick(elapsedTime);
+        this.render();
+    }
+
+    mouseDown(mouseX, mouseY) {
+        this.gangster.vx = mouseX - this.gangster.x;
+        this.gangster.vy = mouseY - this.gangster.y;
+
+        const distance = (this.gangster.vx*this.gangster.vx) + (this.gangster.vy*this.gangster*vy);
+
+        this.gangster.vx/=distance;
+        this.gangster.vy/=distance;
+    }
+
+    tick(elapsedTime) {
+        this.gangster.tick(elapsedTime);
+    }
+
+    render() {
+        this.ctx.fillStyle = "white";
+        this.ctx.fillRect(0,0, this.ctx.canvas.width, this.ctx.canvas.height);
+        this.gangster.render(this.ctx, this.unitSize);
     }
 
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,11 @@
 import KareGang from "./KareGang.js";
 
+
+
 window.onload = function(){
     const canvas = document.getElementById("canvas");
-        let ctx = canvas.getContext("2d");
-        ctx.canvas.width = window.innerHeight-20;
-        ctx.canvas.height = window.innerHeight-20;
-        ctx.strokeRect(0,0,ctx.canvas.width,ctx.canvas.height)
-
-
-        const game = new KareGang();
-        game.start();
+    let ctx = canvas.getContext("2d");
+    const game = new KareGang(ctx);
+    game.setUpEventListeners();
+    game.start();
 }
